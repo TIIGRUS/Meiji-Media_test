5445427608e82cd439ddda1d012f845a17b296bc
TIIGRUS <>
2020-11-05 23:59:59 +0200

    popup logic js and adaptive

--- markup/components/popup/popup.js
+++ markup/components/popup/popup.js
@@ -1,1 +1,15 @@
+// buttons
+let buttonShowPopup = document.querySelector('.button_show_popup');
+let closeButtonPopup = document.querySelector('.button-close_place_popup');
+
+// wrappers
+let popup = document.querySelector('.popup');
+
+// toggle Popup
+function togglePopup() {
+    popup.classList.toggle('popup_is-opened');
+}
+
+buttonShowPopup.addEventListener('click', togglePopup);
+closeButtonPopup.addEventListener('click', togglePopup);
 
