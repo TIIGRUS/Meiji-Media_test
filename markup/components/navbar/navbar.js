9f8a4df0d22cbd85cb553aa8c934a61cb1fd4347
TIIGRUS <>
2020-11-05 23:59:39 +0200

    navbar finieshed added js and style

--- markup/components/navbar/navbar.js
+++ markup/components/navbar/navbar.js
@@ -1,1 +1,13 @@
+// buttons
+let buttonToggleMenu = document.querySelector('.header__menu-toggle');
 
+// wrappers
+let header = document.querySelector('.header');
+
+// toggle menu
+function toggleMenu() {
+    header.classList.toggle('header_menu_is_opened');
+    this.classList.toggle('button_is_active');
+}
+
+buttonToggleMenu.addEventListener('click', toggleMenu);
